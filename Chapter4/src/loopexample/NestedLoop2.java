d3c6abdd9dd31fee8ffbbae03460d260d2950dd2
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-10-07 11:15:25 +0900

    while 반복문 구구단 실습

--- Chapter4/src/loopexample/NestedLoop2.java
+++ Chapter4/src/loopexample/NestedLoop2.java
@@ -4,21 +4,17 @@ public class NestedLoop2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		int dan = 2;
 		int times = 1;
-
+		
 		while (dan <= 9) {
-			
 			times = 1;
 			
 			while (times <= 9) {
-				
-				System.out.println(dan + "X" + times + "=" + dan * times);
+				System.out.println(dan + "X" + times + "=" + (dan*times));
 				times++;
-				
 			}
-			
 			dan++;
 		}
 	}
