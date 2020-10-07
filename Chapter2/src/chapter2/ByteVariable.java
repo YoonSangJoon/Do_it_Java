f017418bb24619231175eeecadb8e0580ed54ddb
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-10-07 10:05:09 +0900

    바이트 자료형 실습

---
+++ Chapter2/src/chapter2/ByteVariable.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class ByteVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte bData = -128;
+		System.out.println(bData);
+		
+		byte bData2 = 127;
+		
+		// byte 자료형이 표현할 수 있는 범위는 -2의 7승 ~ 2의 7승까지
+	}
+
+}
