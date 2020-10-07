0c8c8fc0d2f579616e9462629d80196995afbb45
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-10-07 10:07:17 +0900

    long 변수 실습

---
+++ Chapter2/src/chapter2/VariableEx2.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class VariableEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long num = 12345678900L;
+		
+		// int = 4byte, long = 8byte & 숫자 뒤에 L 붙여서 써야함
+	}
+
+}
