3189caae91d45dbb5f9a6258153e10104400ecc9
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-10-07 10:04:58 +0900

    이진법 십진법 변수 실습

---
+++ Chapter2/src/chapter2/BinaryTest.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 10;
+		int bNum = 0B1010; // 0B : 2Áø¼ö, 8 4 2 0
+		int oNum = 012;
+		int hNum = 0xa;
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(hNum);
+	}
+
+}
