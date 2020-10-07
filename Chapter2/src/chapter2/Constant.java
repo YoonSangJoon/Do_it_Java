e633466411997cdd1166163ecca4cdc2d8a206c4
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-10-07 10:05:39 +0900

    final 자료형 실습

---
+++ Chapter2/src/chapter2/Constant.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final int MAX_NUM = 100;
+		final float PI = 3.14F;
+		
+		final int STUDENT_NUM = 31;
+		int num = 0;
+		if (num == 30) {}
+		
+		System.out.println(STUDENT_NUM);
+	}
+
+}
