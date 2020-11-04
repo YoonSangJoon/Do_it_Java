c1578a8ffc8afabf29ad17dd514238bafec6dbf4
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-11-04 17:08:47 +0900

    다차원 배열 실습

---
+++ Chapter7/src/array/TowDimensionArray.java
@@ -0,0 +1,28 @@
+package array;
+
+public class TowDimensionArray {
+
+	public static void main(String[] args) {
+		
+//		int [][] arr = new int [2] [3];
+		int [][] arr = {
+				{ 1, 2, 3 }, 
+				{ 3, 4, 5 }
+		};
+		
+		System.out.println(arr.length);
+		System.out.println(arr[0].length);
+		System.out.println(arr[1].length);
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.println(arr[i][j]);
+			}
+		}
+		
+		char [] [] alphabets = new char [13] [2];
+		
+
+	}
+
+}
