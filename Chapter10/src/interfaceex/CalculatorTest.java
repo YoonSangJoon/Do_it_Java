46e0b5590e3e057ba41b2c9295d94fbe982afa0f
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-11-08 16:10:56 +0900

    인터페이스 디폴트 메소드 재정의 실습

--- Chapter10/src/interfaceex/CalculatorTest.java
+++ Chapter10/src/interfaceex/CalculatorTest.java
@@ -11,6 +11,11 @@ public class CalculatorTest {
 		
 		System.out.println(calc.add(num1, num2));
 		
+		calc.description();
+		
+		int [] arr = {1,2,3,4,5};
+		int sum = Calc.total(arr);
+		System.out.println(sum);
 		
 	}
 
